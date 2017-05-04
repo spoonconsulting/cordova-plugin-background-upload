3de1bcd21f1941ea4c7905625b4cc8f4cdd08b95
Mevin Dhunnooa <mevin.dhunnooa@sc-mauritius.com>
2017-05-04 14:38:16 +0400

    save upload info on disk for android

--- src/android/FileTransferBackground.java
+++ src/android/FileTransferBackground.java
@@ -8,15 +8,13 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.json.JSONException;
 
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
-import android.net.Uri;
-import android.os.IBinder;
-import android.os.RemoteException;
 import android.util.Log;
 
+
+import com.sromku.simple.storage.SimpleStorage;
+import com.sromku.simple.storage.Storage;
+import com.sromku.simple.storage.helpers.OrderType;
 
 import net.gotev.uploadservice.MultipartUploadRequest;
 import net.gotev.uploadservice.ServerResponse;
@@ -28,28 +26,31 @@ import net.gotev.uploadservice.okhttp.OkHttpStack;
 
 import java.io.File;
 import java.io.IOException;
-import java.lang.reflect.Array;
-import java.net.URI;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 
 public class FileTransferBackground extends CordovaPlugin {
 
+    private final String uploadDirectoryName = "FileTransferBackground";
+    private Storage storage;
 
     @Override
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         try {
 
             if (action.equalsIgnoreCase("initManager")) {
-                this.initManager(args.length() > 0 ? args.get(0).toString(): null);
+                this.initManager(args.length() > 0 ? args.get(0).toString() : null, callbackContext);
             } else {
 
                 final FileTransferSettings payload = new FileTransferSettings((args.get(0)).toString());
 
-                String fileName=payload.filePath.substring(payload.filePath.lastIndexOf("/")+1);
+                String fileName = payload.filePath.substring(payload.filePath.lastIndexOf("/") + 1);
+
 
                 UploadNotificationConfig notificationConfig = new UploadNotificationConfig()
-                        .setTitle("Uploading "+ fileName)
+                        .setTitle("Uploading " + fileName)
                         .setInProgressMessage("Uploading at [[UPLOAD_RATE]] ([[PROGRESS]])")
                         .setErrorMessage("Error while uploading")
                         .setCompletedMessage("Successfully uploaded ");
@@ -80,6 +81,8 @@ public class FileTransferBackground extends CordovaPlugin {
                                 LogMessage("App onError: " + exception);
 
                                 try {
+                                    updateStateForUpload(payload.id, UploadState.FAILED, null);
+
                                     JSONObject errorObj = new JSONObject();
                                     errorObj.put("id", payload.id);
                                     errorObj.put("error", "upload failed");
@@ -96,6 +99,7 @@ public class FileTransferBackground extends CordovaPlugin {
 
                                 try {
                                     LogMessage("server response : " + serverResponse.getBodyAsString());
+                                    updateStateForUpload(payload.id, UploadState.UPLOADED, serverResponse.getBodyAsString());
 
                                     JSONObject objResult = new JSONObject();
                                     objResult.put("id", payload.id);
@@ -124,6 +128,7 @@ public class FileTransferBackground extends CordovaPlugin {
                 }
 
                 request.startUpload();
+                this.createUploadInfoFile(payload.id);
             }
 
 
@@ -146,16 +151,124 @@ public class FileTransferBackground extends CordovaPlugin {
         Log.d("FileTransferBG", message);
     }
 
-    private void initManager(String options) {
-        UploadService.HTTP_STACK = new OkHttpStack();
-        if (options != null) {
-            //initialised global configuration parameters here
-            //JSONObject settings = new JSONObject(options);
+    private void createUploadInfoFile(String fileId) {
+        try {
+            JSONObject upload = new JSONObject();
+            upload.put("id", fileId);
+            upload.put("createdDate", System.currentTimeMillis() / 1000);
+            upload.put("state", UploadState.STARTED);
+
+            storage.createFile(uploadDirectoryName, fileId + ".json", upload.toString());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
+    }
+
+
+    public void updateStateForUpload(String fileId, String state, String serverResponse) {
+        try {
+            String content = storage.readTextFile(uploadDirectoryName, fileId + ".json");
+            if (content != null) {
+                JSONObject uploadJson = new JSONObject(content);
+                uploadJson.put("state", state);
+                if (state == UploadState.UPLOADED) {
+                    uploadJson.put("serverResponse", serverResponse != null ? serverResponse : "");
+                }
+                //delete old file
+                removeUploadInfoFile(fileId);
+                //write updated file
+                storage.createFile(uploadDirectoryName, fileId + ".json", uploadJson.toString());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void removeUploadInfoFile(String fileId) {
+        storage.deleteFile(uploadDirectoryName, fileId + ".json");
+    }
+
+    private ArrayList<JSONObject> getUploadHistory() {
+        ArrayList<JSONObject> previousUploads = new ArrayList<JSONObject>();
+        try {
+
+            List<File> files = storage.getFiles(uploadDirectoryName, OrderType.DATE);
+            for (File file : files) {
+                if (file.getName().endsWith(".json")) {
+                    String content = storage.readTextFile(uploadDirectoryName, file.getName());
+                    if (content != null) {
+                        JSONObject uploadJson = new JSONObject(content);
+                        previousUploads.add(uploadJson);
+                    }
+
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return previousUploads;
+    }
+
+    private void initManager(String options, final CallbackContext callbackContext) {
+        try {
+
+            UploadService.HTTP_STACK = new OkHttpStack();
+
+            storage = SimpleStorage.getInternalStorage(this.cordova.getActivity().getApplicationContext());
+            storage.createDirectory(uploadDirectoryName);
+
+            LogMessage("created working directory ");
+
+            if (options != null) {
+                //initialised global configuration parameters here
+                //JSONObject settings = new JSONObject(options);
+            }
+
+            ArrayList<JSONObject> previousUploads = getUploadHistory();
+            for (JSONObject upload: previousUploads){
+                String state=upload.getString("state");
+                String id=upload.getString("id");
+
+                if (state.equalsIgnoreCase(UploadState.UPLOADED)){
+
+                    JSONObject objResult = new JSONObject();
+                    objResult.put("id", id);
+                    objResult.put("completed", true);
+                    objResult.put("serverResponse", upload.getString("serverResponse"));
+                    PluginResult progressUpdate = new PluginResult(PluginResult.Status.OK, objResult);
+                    progressUpdate.setKeepCallback(true);
+                    callbackContext.sendPluginResult(progressUpdate);
+
+                }else if (state.equalsIgnoreCase(UploadState.FAILED) || state.equalsIgnoreCase(UploadState.STARTED)){
+                    //if the state is STARTED, it means app was killed before the upload was completed
+                    JSONObject errorObj = new JSONObject();
+                    errorObj.put("id", id);
+                    errorObj.put("error", "upload failed");
+                    PluginResult errorResult = new PluginResult(PluginResult.Status.ERROR, errorObj);
+                    errorResult.setKeepCallback(true);
+                    callbackContext.sendPluginResult(errorResult);
+                }
+                //delete upload info on disk
+                removeUploadInfoFile(id);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 }
 
+class UploadState {
+    public static final String UPLOADED = "UPLOADED";
+    public static final String FAILED = "FAILED";
+    public static final String STARTED = "STARTED";
+}
 
 class FileTransferSettings {
 
