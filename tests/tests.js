9af484b7437da1ac7504b2104f4e1175c30b1f04
sc-nick <nick.andrianaharimalala@sc-mauritius.com>
2020-05-14 16:59:56 +0300

    Adding space in object

--- tests/tests.js
+++ tests/tests.js
@@ -212,7 +212,7 @@ exports.defineAutoTests = function () {
       })
 
       it('upload success with put method', function (done) {
-        nativeUploader = FileTransferManager.init({uploadsMethod: 'PUT'}, function (upload) {
+        nativeUploader = FileTransferManager.init({ uploadsMethod: 'PUT' }, function (upload) {
           if (upload.state === 'UPLOADED') {
             expect(upload.id).toBe('file_id')
             expect(upload.statusCode).toBe(200)
