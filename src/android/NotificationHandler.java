d859d7aa367c675986736fa0a2b8bc73f8b4c5a9
Azhar Beebeejaun <azhar.beebeejaun@sc-mauritius.com>
2020-06-23 17:36:35 +0400

    Update NotificationHandler.java

--- src/android/NotificationHandler.java
+++ src/android/NotificationHandler.java
@@ -57,7 +57,6 @@ public class NotificationHandler extends AbstractSingleNotificationHandler {
         for (Map.Entry<String, TaskData> entry : tasks.entrySet()) {
             if (entry.getValue().getStatus() == TaskStatus.InProgress) {
                 inProgress++;
-
                 speed += convertUnitToKbps(
                         entry.getValue().getInfo().getUploadRate().getUnit().name(),
                         entry.getValue().getInfo().getUploadRate().getValue()
@@ -102,18 +101,9 @@ public class NotificationHandler extends AbstractSingleNotificationHandler {
         final String MPS = UploadRate.UploadRateUnit.MegabitPerSecond.name();
 
         float value = 0;
-
-        if (unit == BPS) {
-            value = speed / 8000f;
-        }
-
-        if (unit == KPS) {
-            value = speed / 8;
-        }
-
-        if (unit == MPS) {
-            value = speed * 125;
-        }
+        if (unit == BPS) { value = speed / 8000f; }
+        if (unit == KPS) { value = speed / 8; }
+        if (unit == MPS) { value = speed * 125; }
 
         return value;
     }
@@ -123,13 +113,8 @@ public class NotificationHandler extends AbstractSingleNotificationHandler {
         final String KBPS = "kB/s";
         final String MBPS = "MB/s";
 
-        if (speed >= 1000) {
-            return String.format("%.0f %s", speed / 1000, MBPS);
-        }
-
-        if (speed < 1) {
-            return String.format("%.0f %s", speed * 1000, BPS);
-        }
+        if (speed >= 1000) { return String.format("%.0f %s", speed / 1000, MBPS); }
+        if (speed < 1) { return String.format("%.0f %s", speed * 1000, BPS); }
 
         return String.format("%.0f %s", speed, KBPS);
     }
