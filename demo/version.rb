3e5f8f71b91e6f833d0b279c2b849e6d44094a38
Nirina Razafimahafaly <nirina.razafimahafaly@santoshaconsulting.com>
2020-07-21 20:36:35 +0300

    test/debug build num 5

--- demo/version.rb
+++ demo/version.rb
@@ -7,10 +7,11 @@ class AppVersion
     version_file="#{dir}/version.txt"
     last_commit = Time.parse(`git log -1 --date=iso --format=%cd`)
     last_version=Time.parse(`git log -1 --date=iso --format=%cd #{version_file}`)
-    version = '1.1'
+    version = '0.1'
     version = `cat #{version_file}`.strip if ENV['TRAVIS_BRANCH'] == 'master'
     version = "3.#{ENV['TRAVIS_PULL_REQUEST']}" if ENV['TRAVIS_PULL_REQUEST'] != nil && ENV['TRAVIS_PULL_REQUEST'] != 'false'
     "#{version}.#{(last_commit - last_version).to_i/600}"
   end
 end
+
 puts AppVersion.version
