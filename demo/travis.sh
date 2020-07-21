95ff21681bbacbd98016a26c44336847530862f5
Nirina Razafimahafaly <nirina.razafimahafaly@santoshaconsulting.com>
2020-07-21 13:23:17 +0300

    test/debug build num 5

--- demo/travis.sh
+++ demo/travis.sh
@@ -15,6 +15,7 @@ npm install
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   export ANDROID_HOME=/usr/local/android-sdk
   export ANDROID_SDK_ROOT=/usr/local/android-sdk
+  export PATH=/usr/local/android-sdk/build-tools/28.0.3/:${PATH}
 
   bundle exec fastlane android beta  
 fi
