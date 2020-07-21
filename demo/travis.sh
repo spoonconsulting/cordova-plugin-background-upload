048bed8fc06fe0b68a18d8ecbcbc934ee5ef7104
Nirina Razafimahafaly <nirina.razafimahafaly@santoshaconsulting.com>
2020-07-21 12:16:48 +0300

    test/debug build num 1

--- demo/travis.sh
+++ demo/travis.sh
@@ -1,3 +1,22 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+nvm install 12.14.0
+nvm use 12.14.0
+rvm install 2.7.0
+gem install bundler -v 2.1.4
+bundle install --deployment
+node -v
+npm install --unsafe-perm=true --allow-root -g cordova@8.1.2 ionic@5.4.1
+npm install
+
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    echo "Build android" 
+  export ANDROID_HOME=/usr/local/android-sdk
+  export ANDROID_SDK_ROOT=/usr/local/android-sdk
+  export PATH=/usr/local/android-sdk/tools/:${PATH}
+  export PATH=/usr/local/android-sdk/platform-tools/:${PATH}
+  export PATH=/usr/local/android-sdk/build-tools/28.0.3/:${PATH}
+  
+  bundle exec fastlane android beta  
 fi
