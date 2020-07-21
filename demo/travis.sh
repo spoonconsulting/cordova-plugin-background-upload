9ef10ae49ae7e5616ceb17e0688ccd46b0f80119
Nirina Razafimahafaly <nirina.razafimahafaly@santoshaconsulting.com>
2020-07-21 12:39:41 +0300

    test/debug build num 4

--- demo/travis.sh
+++ demo/travis.sh
@@ -6,6 +6,8 @@ cd demo
 
 gem install bundler -v 2.1.4
 bundle config set deployment 'true'
+bundle install
+
 node -v
 npm install --unsafe-perm=true --allow-root -g cordova@8.1.2 ionic@5.4.1
 npm install
