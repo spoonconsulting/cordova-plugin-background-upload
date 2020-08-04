0360ef839ab93f4f924c46ad7bbeb7a3559fef5c
Nirina Razafimahafaly <nirina.razafimahafaly@santoshaconsulting.com>
2020-08-04 12:34:03 +0300

    Temporary remove ios initialized event

--- src/ios/FileTransferBackground.m
+++ src/ios/FileTransferBackground.m
@@ -16,11 +16,6 @@
         }
 
         [FileUploader sharedInstance].delegate = self;
-
-        [self sendCallback:@{
-            @"state" : @"INITIALIZED"
-        }];
-
         //mark all old uploads as failed to be retried
         for (NSString* uploadId in [self getV1Uploads]){
             [self sendCallback:@{
