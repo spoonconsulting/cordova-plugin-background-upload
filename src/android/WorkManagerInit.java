f2c4e55ad262de07e797127b6f8ac6e25cd40611
zafirskthelifehacker <zafir.heerah@spoonconsulting.com>
2021-11-15 15:15:25 +0400

    Added Default Value for Concurrant Uploads

--- src/android/WorkManagerInit.java
+++ src/android/WorkManagerInit.java
@@ -2,29 +2,48 @@ package com.spoon.backgroundfileupload;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.res.Resources;
 import android.database.Cursor;
 import android.net.Uri;
-import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.work.Configuration;
 import androidx.work.WorkManager;
 
-import com.sharinpix.SharinPix.R;
-
 import java.util.concurrent.Executors;
 
 public class WorkManagerInit extends ContentProvider {
+
+    private static final int defaultConcurrentUploads = 2;
+
     @Override
     public boolean onCreate() {
-        Integer concurrentUploads = Integer.parseInt(String.valueOf(getContext().getApplicationContext().getResources().getText(R.string.concurrentUploads)));
+        int concurrentUploadsIdentifier = getContext().getApplicationContext().getResources().getIdentifier("concurrentUploads", "string", getContext().getApplicationContext().getPackageName());
+        int concurrentUploads;
+        if (concurrentUploadsIdentifier != 0) {
+            concurrentUploads = Integer.parseInt(getContext().getApplicationContext().getResources().getString(concurrentUploadsIdentifier));
+        } else {
+            concurrentUploads = defaultConcurrentUploads;
+        }
+
         Configuration configuration = new Configuration.Builder()
                 .setExecutor(Executors.newFixedThreadPool(concurrentUploads))
                 .build();
 
         WorkManager.initialize(getContext().getApplicationContext(), configuration);
         return true;
+    }
+
+    public static boolean isResource(Context context, int resId){
+        if (context != null){
+            try {
+                return context.getResources().getResourceName(resId) != null;
+            } catch (Resources.NotFoundException ignore) {
+            }
+        }
+        return false;
     }
 
     @Nullable
