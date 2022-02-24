2c07fa413769a3eeb3cc855448ea612f5b6602cc
zafirskthelifehacker <zafir.heerah@spoonconsulting.com>
2022-02-24 15:02:22 +0400

    Remove unused imports

--- src/android/UploadTask.java
+++ src/android/UploadTask.java
@@ -8,7 +8,6 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.util.Log;
 import android.webkit.MimeTypeMap;
