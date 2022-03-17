badab8c7f2a97c80df90e5d6dd39cd9c7c90d932
zafirskthelifehacker <zafir.heerah@spoonconsulting.com>
2022-03-17 15:37:06 +0400

    Remove Executor Import

--- src/android/FileTransferBackground.java
+++ src/android/FileTransferBackground.java
@@ -40,7 +40,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
