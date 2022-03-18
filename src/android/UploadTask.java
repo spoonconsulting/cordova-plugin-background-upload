68f70ca6a172213f8b34dd4404b3f7b91053bb2e
zafirskthelifehacker <zafir.heerah@spoonconsulting.com>
2022-03-18 17:55:24 +0400

    FileInputStream close() and OutputStream close()

--- src/android/UploadTask.java
+++ src/android/UploadTask.java
@@ -268,6 +268,7 @@ public final class UploadTask extends Worker {
 
             try (FileOutputStream fos = getApplicationContext().openFileOutput(filename, Context.MODE_PRIVATE)) {
                 fos.write(res.getBytes(StandardCharsets.UTF_8));
+                fos.close();
             }
 
             outputData.putString(KEY_OUTPUT_RESPONSE_FILE, filename);
@@ -330,7 +331,13 @@ public final class UploadTask extends Worker {
         String extension = MimeTypeMap.getFileExtensionFromUrl(filepath);
         MediaType mediaType = MediaType.parse(MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension));
         File file = new File(filepath);
-        ProgressRequestBody fileRequestBody = new ProgressRequestBody(mediaType, file.length(), new FileInputStream(file), this::handleProgress);
+        FileInputStream fileInputStream = new FileInputStream(file);
+        ProgressRequestBody fileRequestBody = new ProgressRequestBody(mediaType, file.length(), fileInputStream, this::handleProgress);
+        try {
+            fileInputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         // Build body
         final MultipartBody.Builder bodyBuilder = new MultipartBody.Builder();
