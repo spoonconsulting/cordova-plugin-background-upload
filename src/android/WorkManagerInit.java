b7debb11234bbf622098a4a73ced1154f8ceb116
Sk Heerah Zafir <zafir.heerah@spoonconsulting.com>
2022-04-15 09:26:40 +0400

    Add WorkManagerInit

---
+++ src/android/WorkManagerInit.java
@@ -0,0 +1,51 @@
+package com.spoon.backgroundfileupload;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.work.Configuration;
+import androidx.work.WorkManager;
+
+public class WorkManagerInit extends ContentProvider {
+    @Override
+    public boolean onCreate() {
+        Configuration workManagerConfig = new Configuration.Builder()
+                .setMaxSchedulerLimit(100)
+                .build();
+
+        WorkManager.initialize(getContext(), workManagerConfig);
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] strings, @Nullable String s, @Nullable String[] strings1, @Nullable String s1) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+}
