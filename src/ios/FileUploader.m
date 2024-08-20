f9225cd038a1b7b2e63ca221d25d1c7e4140d764
parveshneedhoo <parvesh.needhoo@spoonconsulting.com>
2024-08-20 14:28:07 +0400

    fix migration crash

--- src/ios/FileUploader.m
+++ src/ios/FileUploader.m
@@ -36,14 +36,25 @@ static NSString * kUploadUUIDStrPropertyKey = @"com.spoonconsulting.plugin-backg
             NSData* serverData = weakSelf.responsesData[@(task.taskIdentifier)];
             NSString* serverResponse = serverData ? [[NSString alloc] initWithData:serverData encoding:NSUTF8StringEncoding] : @"";
             [weakSelf.responsesData removeObjectForKey:@(task.taskIdentifier)];
-            [weakSelf saveAndSendEvent:@{
-                @"id" : uploadId,
-                @"state" : @"UPLOADED",
-                @"statusCode" : @(((NSHTTPURLResponse *)task.response).statusCode),
-                @"serverResponse" : serverResponse,
-                @"uploadDuration" : @(duration)
-            }];
+            if (isnumber(duration)) {
+                [weakSelf saveAndSendEvent:@{
+                    @"id" : uploadId,
+                    @"state" : @"UPLOADED",
+                    @"statusCode" : @(((NSHTTPURLResponse *)task.response).statusCode),
+                    @"serverResponse" : serverResponse,
+                    @"uploadDuration" : @(duration)
+                }];
+            } else {
+                [weakSelf saveAndSendEvent:@{
+                    @"id" : uploadId,
+                    @"state" : @"UPLOADED",
+                    @"statusCode" : @(((NSHTTPURLResponse *)task.response).statusCode),
+                    @"serverResponse" : serverResponse,
+                    @"uploadDuration" : @"N/A"
+                }];
+            }
         } else {
+            [weakSelf.responsesData removeObjectForKey:@(task.taskIdentifier)];
             [weakSelf saveAndSendEvent:@{
                 @"id" : uploadId,
                 @"state" : @"FAILED",
